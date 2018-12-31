a403efe8853d3ad30b7c103b74c490ad3eef7041
Tutorial Doctor <TutorialDoctor@users.noreply.github.com>
2018-12-31 14:13:32 -0500

    Update SQLalchemy_Person.py

--- SingleFile/SQLalchemy_Person.py
+++ SingleFile/SQLalchemy_Person.py
@@ -2,13 +2,21 @@ from sqlalchemy import create_engine,ForeignKey,Column,Date,Integer,String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship,backref,sessionmaker
 
-db = "sqlite:///people.db"
-Base = declarative_base()
+db_uri = "sqlite:///people.db"
+db_engine = create_engine(db_uri,echo=True) #echo - prints queries to console
+db_session = sessionmaker(bind=db_engine)
+session = db_session()
+
+Base = declarative_base() #Like d.model in flask_sqlalchemy
 
 class Person(Base):
 	__tablename__ = "person"
 	id = Column(Integer, primary_key=True)
 	name = Column(String(250),nullable=False)
+	
+	def __repr__(self):
+		return str(self.name)
+	#print(instance) - returns the name of the instance
 	
 class Address(Base):
 	__tablename__ = "address"
@@ -17,17 +25,30 @@ class Address(Base):
 	street_number = Column(String(250))
 	post_code = Column(String(250),nullable=False)
 	person_id = Column(Integer,ForeignKey('person.id'))
-	person = relationship(Person)
+	person = relationship(Person,backref='address') #backref allows Person.address
+		
+	def __repr__(self):
+		return str(self.id)
+	#print(instance): returns the id of the instance. 
+	#You could also do: return str(self.street_name)
+		
+#Create tables from class metadata
+Base.metadata.create_all(db_engine) 
 
-engine = create_engine(db,echo=True) #echo prints queries
-Base.metadata.create_all(engine)
-db_session = sessionmaker(bind=engine)
-session = db_session()
+#Create a new person named Joe
+new_person = Person(name='Joe')
 
-new_person = Person(name = 'Joe')
+#Add Joe to the session
 session.add(new_person)
-session.commit()
 
-new_address = Address(post_code = '00000',person=new_person)
+#Commit the session to the database
+session.commit() #this may be redundant
+
+#Create a new address
+new_address = Address(post_code='00000',person=new_person)
 session.add(new_address)
 session.commit()
+
+#Testing
+print(new_person)
+print(new_person.address)
